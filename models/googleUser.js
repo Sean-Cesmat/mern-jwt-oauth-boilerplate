f7a9c7565a238902264996b0ae0fcedb3d472bd0
Sean Cesmat <sean.cesmat@gmail.com>
2018-03-16 16:37:08 -0700

    init commit

---
+++ models/googleUser.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+var googleUserSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true
+  },
+  displayName: {
+    type: String,
+  }
+});
+
+var GoogleUser = mongoose.model('GoogleUser', googleUserSchema);
+
+module.exports = GoogleUser;
